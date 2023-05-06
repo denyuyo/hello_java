d94c0197186aa5349244be63b1c29665755746f2
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-05-06 07:30:15 +0000

    🚿practise java

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-// じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
-// じゃんけんの手をランダムに作成する関数を呼び出す
+null
+alert('またチャレンジしてね')
+
+if (user_hand !== 'グー' && user_hand !== 'チョキ' && user_hand !== 'パー') {
+  alert('グー・チョキ・パーのいずれかを入力してください');
+} else {
+
 let js_hand = getJShand();
 
-// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+
 let judge = winLose(user_hand, js_hand);
 
-// 結果を表示する
+
+
+
 alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
 
-// ランダムでじゃんけんの手を作成する関数
+
 function getJShand(){
   let js_hand_num = Math.floor( Math.random() * 3 );
   let hand_name;
@@ -26,7 +33,7 @@ function getJShand(){
   return hand_name;
 }
 
-// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+
 function winLose(user, js){
   let winLoseStr;
 
@@ -57,4 +64,5 @@ function winLose(user, js){
   }
 
   return winLoseStr;
+}
 }
